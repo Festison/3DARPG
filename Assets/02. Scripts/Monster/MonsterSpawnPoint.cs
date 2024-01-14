54aaae9c569d37143228a3eb2c373cf8ab6a33aa
Festison <sundogdog1958@gmail.com>
2024-01-14 19:46:01 +0900

    전략 패턴 (01-14)

--- Assets/02. Scripts/Monster/MonsterSpawnPoint.cs
+++ Assets/02. Scripts/Monster/MonsterSpawnPoint.cs
@@ -3,21 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public abstract class SpawnStrategy
+public enum MonsterType
 {
-    public abstract void Spawn();
+    MushRoom,
+    Dragon
 }
 
-public class MonsterSpawnPoint : MonoBehaviour
+[System.Serializable]
+public abstract class SpawnStrategy : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    public abstract void OnTriggerStay(Collider other);
+}
+
+[System.Serializable]
+public class MushRoomSpawnStrategy : SpawnStrategy
+{
+    public override void OnTriggerStay(Collider other)
     {
+        Debug.Log("Ãæµ¹Áß");
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (other.TryGetComponent<Player>(out Player player))
             {
                 MonsterObjPool.Instance.PopMonster("Mushroom", Quaternion.identity);
-            }           
-        }    
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class DragonSpawnStrategy : SpawnStrategy
+{
+    public override void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (other.TryGetComponent<Player>(out Player player))
+            {
+                MonsterObjPool.Instance.PopMonster("Dragon", Quaternion.identity);
+            }
+        }
+    }
+}
+
+public class MonsterSpawnPoint : MonoBehaviour
+{
+    [SerializeField] private MonsterType monsterType;
+    [SerializeField] private SpawnStrategy spawnMonster;
+
+    private void Start()
+    {
+        switch (monsterType)
+        {
+            case MonsterType.MushRoom:
+                spawnMonster = new MushRoomSpawnStrategy();
+                break;
+            case MonsterType.Dragon:
+                spawnMonster = new DragonSpawnStrategy();
+                break;
+        }
     }
 }
